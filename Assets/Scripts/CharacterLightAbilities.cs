e9f12bb42a8e34183df0047db56cf77f17f6e4f2
lzbotha <leonardzbotha@gmail.com>
2014-08-14 11:16:43 +0200

    added left trigger to moveDirection lock

--- Assets/Scripts/CharacterLightAbilities.cs
+++ Assets/Scripts/CharacterLightAbilities.cs
@@ -12,8 +12,8 @@ public class CharacterLightAbilities : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Right trigger is down
-		if(Input.GetAxis("ThrowRight") > 0){
+		// Right/Left trigger is down
+		if(Input.GetAxis("ThrowRight") > 0 || Input.GetAxis("ThrowLeft") > 0){
 			// Lock the characters movementDirection
 			characterState.movementDirectionLocked = true;
 		} 
