d96ebf62b9a780388f33350c3247933ff8334bfb
Eduardo Koloma Jr <klmedu001@myuct.ac.za>
2014-08-15 10:30:10 +0200

    Fixed Jump and Smear

--- Assets/SmearChecker.cs
+++ Assets/SmearChecker.cs
@@ -34,8 +34,21 @@ public class SmearChecker : MonoBehaviour {
 		}
 
 		if(target != Vector3.zero){
-			Debug.Log("target not zero = "+Vector3.Distance(transform.parent.position, target));
-			if(Mathf.Abs(transform.parent.position.x - target.x) > 0.01f || Mathf.Abs(transform.parent.position.z - target.z) > 0.01f){
+			Debug.Log("target not zero = "+transform.parent.position+"vs"+target);
+			if(characterState.inAir && (Mathf.Abs(transform.parent.position.x - target.x) > 0.5f || Mathf.Abs(transform.parent.position.z - target.z) > 0.5f)){
+				target.y = transform.parent.position.y;
+				transform.parent.position = Vector3.Lerp(transform.parent.position, target, smearSpeed*Time.deltaTime);
+			}else if(characterState.inAir){
+				target = Vector3.zero;
+				transform.localPosition = Vector3.zero;
+				//change layer of character and all its children
+				Transform [] children = transform.parent.gameObject.GetComponentsInChildren<Transform>();
+				foreach (Transform child in children){
+					child.gameObject.layer = LayerMask.NameToLayer("Default");
+				}
+				transform.parent.gameObject.layer = LayerMask.NameToLayer("Default");
+			}else if(Mathf.Abs(transform.parent.position.x - target.x) > 0.1f || Mathf.Abs(transform.parent.position.z - target.z) > 0.1f){
+				target.y = transform.parent.position.y;
 				transform.parent.position = Vector3.Lerp(transform.parent.position, target, smearSpeed*Time.deltaTime);
 			}else{
 				Debug.Log("changing target to zero");
