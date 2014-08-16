4c2e58c31a7ed937adf31c2670d5da1c296a614a
lzbotha <leonardzbotha@gmail.com>
2014-08-16 08:32:01 +0200

    Added container class for LightShards

---
+++ Assets/Scripts/CharacterLightShardContainer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterLightShardContainer : MonoBehaviour {
+	// Dictionary<string, GameObject> lightShards = new Dictionary<string, GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
