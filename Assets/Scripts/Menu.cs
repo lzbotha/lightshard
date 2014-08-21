acff8a1a683d6f67547e5024ad657355e247620c
lzbotha <leonardzbotha@gmail.com>
2014-08-21 15:17:00 +0200

    added a lot of menu code

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -10,19 +10,43 @@ public class Menu : MonoBehaviour {
 	private const int SETTINGSMENU = 1;
 	private const int CREDITS = 2;
 
+	private string[] mainMenuButtons = new string[5] {"Single Play", "Cooperative Play", "Option", "Credits", "Exit"};
+	private string[] mainMenuClicks = new string[5] {"onSinglePlayClick", "onCooperativePlayClick", "onOptionsClick", "onCreditsClick", "onExitClick"};
+
 	void drawMainMenu(){
-		if (GUI.Button(new Rect(Screen.width * 0.5f - mainMenuButtonWidth * 0.5f, 10, mainMenuButtonWidth, mainMenuButtonHeight), "Single Play")){
-            Application.LoadLevel ("1-tutorial"); 
+		int down = 10;
+		for(int i = 0; i < mainMenuButtons.Length; i++){
+			drawButton(mainMenuButtons[i], Screen.width * 0.5f - mainMenuButtonWidth * 0.5f, down, mainMenuButtonWidth, mainMenuButtonHeight, mainMenuClicks[i]);
+			down += 50;
 		}
-		if(GUI.Button(new Rect(Screen.width * 0.5f - mainMenuButtonWidth * 0.5f, 60, mainMenuButtonWidth, mainMenuButtonHeight), "Cooperative Play")){
-			Debug.Log("coop play");
+	}
+	
+	// TODO: change the last term to a lambda
+	private void drawButton(string name, float x, float y, float width, float height, string onClickMethodName){
+		GUI.SetNextControlName(name);
+		if (GUI.Button(new Rect(x, y, width, height), name)){
+            Invoke(onClickMethodName, 0);
 		}
-		if(GUI.Button(new Rect(Screen.width * 0.5f - mainMenuButtonWidth * 0.5f, 110, mainMenuButtonWidth, mainMenuButtonHeight), "Settings")){
-			currentMenu = SETTINGSMENU;
-		}
-		if(GUI.Button(new Rect(Screen.width * 0.5f - mainMenuButtonWidth * 0.5f, 160, mainMenuButtonWidth, mainMenuButtonHeight), "Credits")){
-			currentMenu = CREDITS;
-		}
+	}
+
+	private void onSinglePlayClick() {
+		Application.LoadLevel ("1-tutorial"); 
+	}
+
+	private void onCooperativePlayClick() {
+		Debug.Log("coop play");
+	}
+
+	private void onOptionsClick(){
+		currentMenu = SETTINGSMENU;
+	}
+
+	private void onCreditsClick(){
+		currentMenu = CREDITS;
+	}
+
+	private void onExitClick(){
+		Debug.Log("There is no leaving... your soul is ours");
 	}
 
 	void drawCredits(){
