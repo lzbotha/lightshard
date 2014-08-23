ad8d6e0bb2ac182b981c0a84c7d466fc3fa59d85
lzbotha <leonardzbotha@gmail.com>
2014-08-23 22:24:43 +0200

    added a hacky version of a mine

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Mine : MonoBehaviour {
+	public float detonateDelay = 0.5f;
+	public float detectionRadius = 3.0f;
+	public float armingRadius = 3.0f;
+
+	private GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public abstract void onArm();
+
+	public abstract void onDetection();
+
+	void detonate(){
+		Vector3 direction = obj.transform.position - this.transform.position;
+		direction.Normalize();
+		obj.GetComponent<CharacterMovement>().applyVelocity(direction * 30);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player"){
+			obj = other.gameObject;
+			Invoke("detonate", detonateDelay);
+		}
+	}
+}
---
+++ Assets/Scripts/MushroomMine.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MushroomMine : Mine {
+
+	public override void onArm(){
+
+	}
+
+	public override void onDetection(){
+
+	}
+}
