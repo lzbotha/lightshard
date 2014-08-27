a33542806cbc4c0a79096b088846ab1298a45671
lzbotha <leonardzbotha@gmail.com>
2014-08-27 13:35:17 +0200

    added a trampoline script
    the vector maths required for this is tricky

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : MonoBehaviour {
+
+	// all tags that can bounce on this trampoline
+	public string[] bouncableTags;
+
+	void OnTriggerEnter(Collider other){
+		// The vector maths for this is tricky....
+		
+	}
+
+}
