bebc98a8de780eac1ef5e3aad0f9c502b08c5c94
lzbotha <leonardzbotha@gmail.com>
2014-08-27 14:08:04 +0200

    added trampoline logic

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -7,8 +7,17 @@ public class Trampoline : MonoBehaviour {
 	public string[] bouncableTags;
 
 	void OnTriggerEnter(Collider other){
-		// The vector maths for this is tricky....
-		
+		BasicState state = other.gameObject.GetComponent<BasicState>();
+		Vector3 velocity = state.getContinuousVelocity()
+		+ new Vector3(0, state.getVerticalSpeed(), 0);
+
+		Vector3 up = this.transform.up;
+
+		Vector3 vdash = Vector3.Dot(velocity, up)/Vector3.Dot(up, up) * up;
+
+		state.setVerticalSpeed(state.getVerticalSpeed() - 2 * vdash.y);
+		vdash.y = 0;
+		state.setContinuousVelocity(state.getContinuousVelocity() - 2 * vdash);
 	}
 
 }
