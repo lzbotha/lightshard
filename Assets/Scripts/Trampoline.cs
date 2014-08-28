7e372b81b1b3dd0db2500e4f55a4e33a8569b1a2
lzbotha <leonardzbotha@gmail.com>
2014-08-28 12:14:26 +0200

    renamed variable

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -7,15 +7,14 @@ public class Trampoline : MonoBehaviour {
 	public string[] bouncableTags;
 
 	void OnTriggerEnter(Collider other){
-		print("bounce");
 		BasicState state = other.gameObject.GetComponent<BasicState>();
+		
 		Vector3 velocity = state.getVelocity();
-
 		Vector3 up = this.transform.up;
 
-		Vector3 vdash = Vector3.Dot(velocity, up)/Vector3.Dot(up, up) * up;
+		Vector3 deltaVelocity = Vector3.Dot(velocity, up)/Vector3.Dot(up, up) * up;
 
-		state.setVelocity(state.getVelocity() - 2 * vdash);
+		state.setVelocity(state.getVelocity() - 2 * deltaVelocity);
 	}
 
 }
