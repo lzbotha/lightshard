066a228b93b7ba8c5fa2a71f50fff1d7deb856a8
lzbotha <leonardzbotha@gmail.com>
2014-09-12 11:21:15 +0200

    added effect to player activation of binfires

--- Assets/Scripts/BonfireController.cs
+++ Assets/Scripts/BonfireController.cs
@@ -3,9 +3,29 @@ using System.Collections;
 
 public class BonfireController : MonoBehaviour {
 
+	public ParticleSystem characterActivationEffect;
+
+	void Start() {
+		this.characterActivationEffect.Stop();
+	}
+
+	void characterActivationStart(GameObject player) {
+		// Play bonfire activation effect and set the players respawn point to this bonfire
+		this.characterActivationEffect.Play();
+		Invoke("characterActivationStop", 3);
+		player.GetComponent<CharacterState>().setRespawnPosition(this.transform.position);
+	}
+
+	void characterActivationStop() {
+		this.characterActivationEffect.Stop();
+	}
+
 	void OnTriggerEnter(Collider other){
 		if(other.tag == "Player"){
-			other.gameObject.GetComponent<CharacterState>().setRespawnPosition(this.transform.position);
+			// TODO: change this to work with distance
+			if(other.transform.position != this.transform.position){
+				characterActivationStart(other.gameObject);
+			}
 		}
 	}
 }
