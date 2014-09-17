f44c2ece9c66b2f01c34e818a5f8348a5dea0b55
lzbotha <leonardzbotha@gmail.com>
2014-09-17 11:20:11 +0200

    very basic chasing now working

--- Assets/Scripts/AgentMovement.cs
+++ Assets/Scripts/AgentMovement.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class AgentMovement : BasicMovement {
+	public AgentState agentState;
 
-	// Use this for initialization
-	void Start () {
-	
+	private Vector3 movementComponent = Vector3.zero;
+
+	public void chaseTarget(Vector3 targetPosition){
+		agentState.findPathTo (targetPosition);
+
+		// remove previous iterations movement component
+		agentState.setVelocity(agentState.getVelocity() - this.movementComponent);
+
+		// if the agent is on the ground apply friction to non movementComponent velocities
+		if(this.controller.isGrounded)
+			applyFriction();
+
+		// calculate new movement component
+		Vector3 direction = (this.agentState.getNextWaypoint () - this.transform.position);
+		direction.Normalize();
+
+		movementComponent = direction * this.speed;
+
+		this.agentState.setVelocity (this.agentState.getVelocity () + movementComponent);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void move(){
+		print (agentState.getVelocity () * Time.deltaTime);
+		this.controller.Move (agentState.getVelocity() * Time.deltaTime);
 	}
 }
--- Assets/Scripts/AgentState.cs
+++ Assets/Scripts/AgentState.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class AgentState : BasicState {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Pathfinding pf;
+	public float distToWaypoint = 0.1f;
+
+	public void findPathTo(Vector3 target){
+		pf.FindPath(this.transform.position, target);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public Vector3 getNextWaypoint(){
+		// if there are waypoints remaining in the path
+		if (pf.Path.Count > 0) {
+			Vector3 path = pf.Path [0];
+
+			// if this agent is within distToWaypoint of the current waypoint remove it from the path
+			if (Vector3.Distance (this.transform.position, pf.Path [0]) < this.distToWaypoint) {
+				pf.Path.RemoveAt (0);
+			}
+
+			return path;
+		}
+		return Vector3.zero;
 	}
 }
