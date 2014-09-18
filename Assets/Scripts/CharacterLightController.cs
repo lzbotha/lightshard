259f7f231099bc663803ca0413cda4cff912b6b1
lzbotha <leonardzbotha@gmail.com>
2014-09-18 13:45:38 +0200

    minor refactoring

--- Assets/Scripts/CharacterLightController.cs
+++ Assets/Scripts/CharacterLightController.cs
@@ -12,14 +12,14 @@ public class CharacterLightController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		RenderSettings.ambientLight = Color.black;
-		characterState.setLightRegenRate(normalLightRegenRate);
-		characterState.setLightRadius(normalLightRadius);
-		characterState.setMinLightRadius(minLightRadius);
+		this.characterState.setLightRegenRate(normalLightRegenRate);
+		this.characterState.setLightRadius(normalLightRadius);
+		this.characterState.setMinLightRadius(minLightRadius);
 	}
 
 	public void disableFlash() {
-		characterState.setLightRegenRate(normalLightRegenRate);
-		characterState.setFlashDeactivationRadius(0);
+		this.characterState.setLightRegenRate(this.normalLightRegenRate);
+		this.characterState.setFlashDeactivationRadius(0);
 	}
 
 	public void restoreLightRadius() {
@@ -30,12 +30,12 @@ public class CharacterLightController : MonoBehaviour {
 		// If a Flash is in progress
 		// Both a check for negative regen and a non zero flashDeactivationRadius are necessary
 		// incase the player has a negative light regen rate because of level stuff
-		if(characterState.isLightRegenNegative() && characterState.getFlashDeactivationRadius() > 0){
+		if(this.characterState.isLightRegenNegative() && this.characterState.getFlashDeactivationRadius() > 0){
 			// Decrease light radius
-			characterState.changeLightRadiusBy(characterState.getLightRegenRate() * Time.deltaTime);
+			this.characterState.changeLightRadiusBy(this.characterState.getLightRegenRate() * Time.deltaTime);
 			// If the flash duration is over, or the character is at minimum light 
 			// radius, restore the characters light regen to normal
-			if(characterState.getLightRadius() <= characterState.getFlashDeactivationRadius()){
+			if(this.characterState.getLightRadius() <= this.characterState.getFlashDeactivationRadius()){
 				this.disableFlash();
 			}
 		}
@@ -44,10 +44,9 @@ public class CharacterLightController : MonoBehaviour {
 		// 			can't be in an else
 		else if (characterState.isLightRegenPositive()) {
 			// Restore the characters light radius up to the normal value
-			if(characterState.getLightRadius() + characterState.getLightRegenRate() * Time.deltaTime <= normalLightRadius)
-				characterState.changeLightRadiusBy(characterState.getLightRegenRate() * Time.deltaTime);
-			//characterState.setLightRadius(Mathf.Clamp(characterState.getLightRadius() + characterState.getLightRegenRate() * Time.deltaTime, characterState.getMinLightRadius(), normalLightRadius));
+			if(this.characterState.getLightRadius() + this.characterState.getLightRegenRate() * Time.deltaTime <= this.normalLightRadius)
+				this.characterState.changeLightRadiusBy(this.characterState.getLightRegenRate() * Time.deltaTime);
 		}
-		gameObject.light.range = characterState.getLightRadius();
+		this.gameObject.light.range = characterState.getLightRadius();
 	}
 }
