efbc1830d088ce4fa2da27d40b1175955c7c0eb4
lzbotha <leonardzbotha@gmail.com>
2014-09-21 09:03:31 +0200

    started work on chase agent

---
+++ Assets/Scripts/ChaseAgentState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseAgentState : MonoBehaviour {
+
+	private GameObject targetPlayer;
+
+	void OnTriggerEnter(Collider other){
+
+	}
+
+	void OnTriggerExit(Collider other){
+
+
+	}
+}
