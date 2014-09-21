872d75b482c11b9e445f0c7b50f68777d9b4e5a6
lzbotha <leonardzbotha@gmail.com>
2014-09-21 09:07:42 +0200

    added some basic chase logic

--- Assets/Scripts/ChaseAgentState.cs
+++ Assets/Scripts/ChaseAgentState.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class ChaseAgentState : MonoBehaviour {
 
+	public float maxChaseSeperation;
+
 	private GameObject targetPlayer;
+	private bool chasing = false;
 
 	void OnTriggerEnter(Collider other){
-
+		// if this agent is not chasing a player and a player enters its observation range
+		if (!this.chasing && other.tag == "Player") {
+			this.targetPlayer = other.gameObject;
+		}
 	}
 
-	void OnTriggerExit(Collider other){
-
-
+	void Update(){
+		if (chasing) {
+			if(Vector3.Distance(this.transform.position, targetPlayer.transform.position) > maxChaseSeperation){
+				chasing = false;
+			}
+		}
 	}
 }
