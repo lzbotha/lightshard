80ea79d22ffaf4eb2548f91f8e06e6809c7df69a
lzbotha <leonardzbotha@gmail.com>
2014-10-10 10:45:42 +0200

    setup method stubs for all buttons on InGameMenu

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -3,19 +3,28 @@ using System.Collections;
 
 public class InGameMenu : MonoBehaviour {
 
+	public float buttonHeight = 75.0f;
+	public float buttonWidth = 150.0f;
+	public float gap = 10.0f;
+
 	bool isPaused() {
 		return Time.timeScale == 0;
 	}
 
 	void OnGUI () {
 		if (this.isPaused()) {
-			if(GUI.Button(new Rect(Screen.width * 0.5f , 160, 100, 100), "Back")){
+
+			if(GUI.Button(new Rect((Screen.width - buttonWidth) * 0.5f , Screen.height * 0.5f - 2 * (buttonHeight + gap), buttonWidth, buttonHeight), "Continue")){
 				Debug.Log("Button working");
 				Time.timeScale = 1;
 			}
 
-			if (Input.GetButtonDown("Pause")) {
-				Time.timeScale = 1;
+			if(GUI.Button(new Rect((Screen.width - buttonWidth) * 0.5f , Screen.height * 0.5f - (buttonHeight + gap), buttonWidth, buttonHeight), "Quit to Main Menu")){
+
+			}
+
+			if(GUI.Button(new Rect((Screen.width - buttonWidth) * 0.5f , Screen.height * 0.5f, buttonWidth, buttonHeight), "Quit to Desktop")){
+				
 			}
 		}
 	}
