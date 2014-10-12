72a80ebdf44104ea3cff6e2e7da3ec3dfb0a3c81
lzbotha <leonardzbotha@gmail.com>
2014-10-12 18:36:15 +0200

    Added sound, fuck yeah!

---
+++ Assets/Scripts/BackroundPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class BackroundPlayer : MonoBehaviour {
+	public AudioClip[] clips;
+
+	IEnumerator loopAllClips() {
+		while (true) {
+			if (!(this.clips.Length > 0)) {
+				throw new System.Exception("Need audio clips for backround player.");
+			}
+
+			foreach (AudioClip clip in this.clips) {
+				this.audio.clip = clip;
+				this.audio.Play();
+				yield return new WaitForSeconds(clip.length);
+			}
+		}
+	}
+
+	void Start() {
+		StartCoroutine(loopAllClips());
+	}
+
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/MeanPosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeanPosition : MonoBehaviour {
+	
+	public Transform[] transforms;
+
+	// Update is called once per frame
+	void Update () {		
+		Vector3 average = Vector3.zero;
+		foreach (Transform transform in this.transforms) {
+			average += transform.position;
+		}
+
+		average /= this.transforms.Length;
+
+		this.transform.position = average;
+	}
+}
