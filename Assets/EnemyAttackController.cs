da0078d7fb01c339a1b6277d0bd26f8b3eb9df24
Eduardo Koloma Jr <klmedu001@myuct.ac.za>
2014-10-13 00:58:36 +0200

    Added Aggressive Chase Agent Attack

---
+++ Assets/EnemyAttackController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttackController : MonoBehaviour {
+	[SerializeField]
+	GameObject Explosion;
+	CharacterState characterState;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Player"){
+			characterState = other.gameObject.GetComponentInParent<CharacterState>();
+			characterState.damage(3.0f);
+		}
+		GameObject att = Instantiate(Explosion, this.transform.position, Quaternion.identity) as GameObject;
+		att.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/AggressiveChaserAttackBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AggressiveChaserAttackBehaviour : MonoBehaviour {
+	[SerializeField]
+	GameObject enemyAttack;
+	[SerializeField]
+	float attackPeriod;
+	[SerializeField]
+	float attackSpeed;
+	float timer = 0.0f;
+	Transform player;
+	SphereCollider sphereCollider;
+	// Use this for initialization
+	void Start () {
+		sphereCollider = transform.GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player != null && Vector3.Distance(transform.position, player.position) < sphereCollider.radius && timer <= attackPeriod){
+			timer += Time.deltaTime;
+		}
+
+		if(player != null && Vector3.Distance(transform.position, player.position) < sphereCollider.radius && timer >= attackPeriod){
+			GameObject attack = Instantiate(enemyAttack, this.transform.position, Quaternion.LookRotation(player.position)) as GameObject;
+			Vector3 toPlayer = player.position - transform.position;
+			toPlayer.Normalize();
+			attack.rigidbody.AddForce(toPlayer*attackSpeed);
+			timer = 0.0f;
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player"){
+			player = other.gameObject.transform;
+		}
+	}
+}
