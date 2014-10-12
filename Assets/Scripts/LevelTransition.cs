03f1c8db6bb869c8c8ba002dd68203d306885e67
lzbotha <leonardzbotha@gmail.com>
2014-10-13 01:10:20 +0200

    refactoring + level transitions now present in 1-3

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTransition : MonoBehaviour {
+
+	public string levelName;
+	public float delay = 0.0f;
+
+	void OnTriggerEnter(Collider other){
+
+		if(other.tag == "Player")
+			Invoke("transition", delay);
+	}
+
+	private void transition(){
+		Application.LoadLevel(levelName);
+	}
+}
