d39f4dab71eeb201e9700c3b396abb5bed7e56f9
lzbotha <leonardzbotha@gmail.com>
2014-10-13 00:36:57 +0200

    added lightning strike feedback

---
+++ Assets/EffectTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectTrigger : MonoBehaviour {
+
+	public ParticleSystem effect;
+
+	void Start(){
+		effect.Stop ();
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if(other.tag == "Player"){
+			effect.Play();
+		}
+	}
+}
