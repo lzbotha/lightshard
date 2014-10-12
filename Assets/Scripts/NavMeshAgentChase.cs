132d997d1a81e857851e65288acf0bf34a66d979
Eduardo Koloma Jr <klmedu001@myuct.ac.za>
2014-10-12 14:42:59 +0200

    created a simple level with navmesh with agents

---
+++ Assets/Scripts/NavMeshAgentChase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshAgentChase : MonoBehaviour {
+	[SerializeField]
+	Transform target;
+	NavMeshAgent agent;
+	bool targetSet = false;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(target.position, transform.position) < 10){
+			Debug.Log("close enough");
+			agent.SetDestination(target.position);
+		}else{
+			Debug.Log("too far - "+Vector3.Distance(target.position, transform.position));
+			agent.SetDestination(transform.position);
+		}
+	}
+}
