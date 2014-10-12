27f2d36d91c6397a764a78149144fe2ab05dbe94
lzbotha <leonardzbotha@gmail.com>
2014-10-12 14:44:00 +0200

    removed all traces of old pathfinding from agents

--- Assets/Scripts/AgentState.cs
+++ Assets/Scripts/AgentState.cs
@@ -3,29 +3,6 @@ using System.Collections;
 
 public class AgentState : BasicState {
 
-	public Pathfinding pf;
-	public float distToWaypoint = 0.1f;
-
-	public void findPathTo(Vector3 target){
-		print ("findPathTo method called");
-		pf.FindPath(this.transform.position, target);
-	}
-
-	public Vector3 getNextPathSegment(){
-		print ("getNextPathSegment method called");
-		// if there are waypoints remaining in the path
-		if (pf.Path.Count > 0) {
-			Vector3 path = pf.Path [0];
-
-			// if this agent is within distToWaypoint of the current waypoint remove it from the path
-			if (Vector3.Distance (this.transform.position, pf.Path [0]) < this.distToWaypoint) {
-				pf.Path.RemoveAt (0);
-			}
-			return path;
-		}
-		return this.transform.position;
-	}
-
 	void Start(){
 		this.setRespawnPosition (this.transform.position);
 	}
