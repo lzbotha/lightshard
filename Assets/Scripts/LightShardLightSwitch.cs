a9affa06bc48d68d57121922bb5a51485278118a
lzbotha <leonardzbotha@gmail.com>
2014-10-12 22:11:27 +0200

    added lightswitch

---
+++ Assets/Scripts/LightShardLightSwitch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightShardLightSwitch : MonoBehaviour {
+
+	public Light spotLight;
+	private bool isOn = false;
+
+	void OnTriggerEnter(Collider other){
+		if (!this.isOn){
+			print (other.tag);
+			if (other.tag == "LightShard") {
+				print("Lights on");
+				spotLight.intensity = 8.0f;
+				this.isOn = true;
+			}
+		}
+	}
+}
